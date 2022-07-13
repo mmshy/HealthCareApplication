b486397694d45303494b2667f5bd879f863917fb
Huyenne <nguyenkhanhhuyen051122@gmail.com>
2022-07-13 22:18:20 +0700

    Add Icon, Image

--- app/src/main/java/com/example/healthcareapplication/MainActivity.kt
+++ app/src/main/java/com/example/healthcareapplication/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.healthcareapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
@@ -17,14 +18,11 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.fontResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.AndroidViewModel
-import com.example.healthcareapplication.presentation.ui.theme.Blue
 import com.example.healthcareapplication.presentation.ui.theme.HealthCareApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -93,8 +91,6 @@ fun ScrollBoxes() {
 @Composable
 fun DefaultPreview() {
     HealthCareApplicationTheme {
-        androidx.compose.material.MaterialTheme {
-            Greeting()
-        }
+        Image(painterResource(id = R.drawable.shark_sleep), "")
     }
 }
